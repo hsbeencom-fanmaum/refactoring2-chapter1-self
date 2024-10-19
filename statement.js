131793547c71b5ef9ecd39b38530c1b3a4b4dc8f
hsbeencom-fanmaum <140990483+hsbeencom-fanmaum@users.noreply.github.com>
2024-10-19 13:01:10 +0900

    계산 로직 함수로 분리

--- statement.js
+++ statement.js
@@ -10,30 +10,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy": // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    let thisAmount = calculateAmount(play.type, perf.audience);
+    volumeCredits += calculateCredits(play.type, perf.audience);
 
     // 청구 내역을 출력한다.
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
@@ -44,6 +22,36 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+}
+
+function calculateAmount(type, audience) {
+  let thisAmount = 0;
+  switch (type) {
+    case "tragedy": // 비극
+      thisAmount = 40000;
+      if (audience > 30) {
+        thisAmount += 1000 * (audience - 30);
+      }
+      break;
+    case "comedy": // 희극
+      thisAmount = 30000;
+      if (audience > 20) {
+        thisAmount += 10000 + 500 * (audience - 20);
+      }
+      thisAmount += 300 * audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${type}`);
+  }
+  return thisAmount;
+}
+
+function calculateCredits(type, audience) {
+  // 포인트를 적립한다.
+  let volumeCredits = Math.max(audience - 30, 0);
+  // 희극 관객 5명마다 추가 포인트를 제공한다.
+  if ("comedy" === type) volumeCredits += Math.floor(audience / 5);
+  return volumeCredits;
 }
 
 module.exports = { statement };
