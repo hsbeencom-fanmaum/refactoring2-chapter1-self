678fd8332480e0e759291eaa07ae6e7f0c853cc6
hsbeencom-fanmaum <140990483+hsbeencom-fanmaum@users.noreply.github.com>
2024-10-20 00:09:04 +0900

    전체 로직을 클래스로 추출

--- statement.js
+++ statement.js
@@ -1,45 +1,11 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = calculateAmount(play.type, perf.audience);
-    volumeCredits += calculateCredits(play.type, perf.audience);
-    totalAmount += thisAmount;
-
-    // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${formatUSD(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-  }
-  result += `총액: ${formatUSD(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
+  const invoiceGenerator = new InvoiceGenerator(invoice, plays);
+  return invoiceGenerator.renderPlainText();
 }
 
 function htmlStatement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `<h1>청구 내역 (고객명: ${invoice.customer})</h1>\n`;
-  result += "<table>\n";
-  result += "  <tr><th>연극</th><th>좌석수</th><th>금액</th></tr>\n";
-
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = calculateAmount(play.type, perf.audience);
-    volumeCredits += calculateCredits(play.type, perf.audience);
-    totalAmount += thisAmount;
-
-    // 청구 내역을 출력한다.
-    result += `  <tr><td>${play.name}</td><td>(${perf.audience}석)</td>`;
-    result += `<td>${formatUSD(thisAmount / 100)}</td></tr>\n`;
-  }
-  result += "</table>\n";
-  result += `<p>총액: <em>${formatUSD(totalAmount / 100)}</em></p>\n`;
-  result += `<p>적립 포인트: <em>${volumeCredits}점</em></p>\n`;
-  return result;
+  const invoiceGenerator = new InvoiceGenerator(invoice, plays);
+  return invoiceGenerator.renderHtmlText();
 }
 
 function calculateAmount(type, audience) {
@@ -78,6 +44,61 @@ function formatUSD(value) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(value);
+}
+
+class InvoiceGenerator {
+  constructor(invoice, plays) {
+    this.invoice = invoice;
+    this.plays = plays;
+    this.#clearInovice();
+  }
+
+  #clearInovice() {
+    this.totalAmount = 0;
+    this.volumeCredits = 0;
+  }
+
+  renderPlainText() {
+    this.#clearInovice();
+    let result = `청구 내역 (고객명: ${this.invoice.customer})\n`;
+
+    for (let perf of this.invoice.performances) {
+      const play = this.plays[perf.playID];
+      let thisAmount = calculateAmount(play.type, perf.audience);
+      this.volumeCredits += calculateCredits(play.type, perf.audience);
+      this.totalAmount += thisAmount;
+
+      // 청구 내역을 출력한다.
+      result += `  ${play.name}: ${formatUSD(thisAmount / 100)} (${
+        perf.audience
+      }석)\n`;
+    }
+    result += `총액: ${formatUSD(this.totalAmount / 100)}\n`;
+    result += `적립 포인트: ${this.volumeCredits}점\n`;
+    return result;
+  }
+
+  renderHtmlText() {
+    this.#clearInovice();
+    let result = `<h1>청구 내역 (고객명: ${this.invoice.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "  <tr><th>연극</th><th>좌석수</th><th>금액</th></tr>\n";
+
+    for (let perf of this.invoice.performances) {
+      const play = this.plays[perf.playID];
+      let thisAmount = calculateAmount(play.type, perf.audience);
+      this.volumeCredits += calculateCredits(play.type, perf.audience);
+      this.totalAmount += thisAmount;
+
+      // 청구 내역을 출력한다.
+      result += `  <tr><td>${play.name}</td><td>(${perf.audience}석)</td>`;
+      result += `<td>${formatUSD(thisAmount / 100)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${formatUSD(this.totalAmount / 100)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${this.volumeCredits}점</em></p>\n`;
+    return result;
+  }
 }
 
 module.exports = { statement, htmlStatement };
